712fd29a1784cf8070d0c6b4fdb70a9ab0b8c3d4
zoroONE01 <lekimnguyen671@gmail.com>
2022-05-06 14:50:30 +0700

    first commit

---
+++ app/src/main/java/vn/edu/ptithcm/bankmanagement/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ptithcm.bankmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+}
